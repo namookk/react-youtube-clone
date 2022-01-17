f45b4ef8cc7f99061a9727986d6eebd481029a42
namookk <papakang22@naver.com>
2022-01-17 14:55:17 +0900

    add : 6일차 과제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import youtubeData from './data/youtubeData.json';
 
 function App() {
+  const data = youtubeData.data[0];
+  const keys = Object.keys(data);
+  const desc = {
+    "category" : "영상의 카테고리",
+    "channelId" : "채널의 고유값",
+    "channelThumbnail" : "채널 썸네일 이미지 주소",
+    "channelTitle" : "채널 타이틀",
+    "channelUrl" : "채널 Url Id값",
+    "date" : "업로드 날짜 및 시간",
+    "description" : "동영상 설명문구",
+    "id" : "동영상 고유값",
+    "likeCount" : "동영상 좋아요 수",
+    "thumbnail" : "동영상 썸네일",
+    "title" : "동영상 타이틀",
+    "viewCount" : "동영상 조회 수"
+  }
+  for(const key of keys){
+    console.log(key +" : " + data[key] + " -> "+desc[key]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>시작</div>
   );
 }
 
