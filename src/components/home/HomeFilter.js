823422b963538170f18d15d3b75a0f34f613fe84
namookk <papakang22@naver.com>
2022-01-26 12:29:28 +0900

    add : 13일차 과제

--- src/components/home/HomeFilter.js
+++ src/components/home/HomeFilter.js
@@ -1,25 +1,14 @@
 import styles from './HomeFilter.module.css';
 
-const filterArr = ['', 'BTS', 'LISA', '아이폰'];
-
-function HomeFilter({clickfn, filter}){
+function HomeFilter({ filter, text, onClickFilter }) {
     return (
-        <div className={styles.container}>
-            <ul className={styles.filterul}>
-                {filterArr.map((value, idx) => {
-                    return (
-                        <li
-                        key={`home-filter-${idx}`}
-                        className={[styles.filterli, filter === value ? styles.focus : ''].join(' ')}
-                        onClick={() => clickfn(value)}
-                        >
-                         {value == '' ? '전체' : value}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    )
+        <button
+            className={filter === text ? styles.black : styles.gray}
+            onClick={onClickFilter}
+        >
+            {text}
+        </button>
+    );
 }
 
 export default HomeFilter;
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -4,8 +4,16 @@ import { FiMenu } from 'react-icons/fi';
 import { IoSearchOutline } from 'react-icons/io5';
 import { BsGrid3X3Gap } from 'react-icons/bs';
 import { HiOutlineDotsVertical } from 'react-icons/hi';
+import {useState} from "react";
 
 function Header({clickfn}) {
+
+    const [searchKey, setSearchKey] = useState('');
+    function search(){
+        console.log(searchKey);
+        setSearchKey('');
+    }
+
     return (
         <div className={styles.header}>
             <div className={styles.tab}>
@@ -13,8 +21,21 @@ function Header({clickfn}) {
                 <img src={youtube_logo} alt="로고" className={styles.logo} />
             </div>
             <div className={styles['center-tab']}>
-                <input className={styles.input} />
-                <IoSearchOutline className={styles['search-icon']} />
+                <input
+                    className={styles.input}
+                    onChange={(event) => {
+                        setSearchKey(event.target.value);
+                    }}
+                    value={searchKey}
+                    onKeyUp={(e) => {
+                        if(e.keyCode == 13){
+                            search();
+                        }
+                    }}
+                />
+                <IoSearchOutline className={styles['search-icon']}
+                    onClick={search}
+                />
             </div>
             <div className={styles.tab}>
                 <BsGrid3X3Gap className={styles.icon} />
